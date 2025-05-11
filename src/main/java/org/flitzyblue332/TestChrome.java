d6b4b1de077b7ae427fa17d81cbbbc63b352a7b5
FlitzyBlue332 <raihanaulia332@gmail.com>
2025-05-11 13:02:04 +0700

    Changed email reference to thread ID

--- src/main/java/org/flitzyblue332/TestChrome.java
+++ src/main/java/org/flitzyblue332/TestChrome.java
@@ -9,12 +9,12 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.Reporter;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Objects;
 
 public class TestChrome {
     private WebDriver driver;
@@ -28,22 +28,35 @@ public class TestChrome {
         options.addArguments("--incognito");
         driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        driver.get("https://mail.google.com");
+        handleLogin();
     }
 
     @Test
     public void testGmailDelete(){
-        driver.get("https://mail.google.com");
-        handleLogin();
-
-
         WebElement lastUnreadEmail = findLastUnreadEmail();
-        String lastSubject = getEmailSubject(lastUnreadEmail);
-        Reporter.log("Subject: " + lastSubject);
-        System.out.println("Subject: " + lastSubject);
+        String lastThreadID = getEmailID(lastUnreadEmail);
+        Reporter.log("Subject: " + getEmailSubject(lastUnreadEmail));
+        System.out.println("Subject: " + lastThreadID);
         deleteEmail(lastUnreadEmail);
-        verifyEmailDeletion(lastSubject);
-//        verifyEmailDeletedInTrash(lastSubject);
+//        verifyEmailDeletion(lastThreadID);
+        navigateToTrash();
+        verifyEmailDeletedInTrash(lastThreadID);
     }
+
+    @Test
+    public void testGmailDeleteCheckInbox(){
+        driver.get("https://mail.google.com");
+        WebElement lastUnreadEmail = findLastUnreadEmail();
+        String lastThreadID = getEmailID(lastUnreadEmail);
+        Reporter.log("Subject: " + getEmailSubject(lastUnreadEmail));
+        System.out.println("Subject: " + lastThreadID);
+        deleteEmail(lastUnreadEmail);
+        verifyEmailDeletion(lastThreadID);
+//        navigateToTrash();
+//        verifyEmailDeletedInTrash(lastThreadID);
+    }
+
 
     private void handleLogin(){
         // handle login
@@ -63,10 +76,10 @@ public class TestChrome {
 
         // handle otp
         List<WebElement> otpElements = driver.findElements(By.cssSelector("div[data-challenge='TOTP']"));
-        if(otpElements.isEmpty()) {
+        if(!otpElements.isEmpty()) {
             // found otp -> manual input, no other choice
             System.out.println("Manual OTP input required - waiting 20 seconds");
-            wait.until(ExpectedConditions.invisibilityOfElementLocated(
+            wait.withTimeout(Duration.ofSeconds(20)).until(ExpectedConditions.invisibilityOfElementLocated(
                     By.cssSelector("div[data-challenge='TOTP']")));
         }
     }
@@ -85,6 +98,10 @@ public class TestChrome {
         return subjectElement.getText().trim();
     }
 
+    private String getEmailID(WebElement emailElement){
+        return emailElement.findElement(By.cssSelector(".bqe")).getDomAttribute("data-thread-id");
+    }
+
     private void deleteEmail(WebElement emailElement){
         emailElement.findElement(By.cssSelector("div.oZ-jc")).click();
         wait.until(ExpectedConditions.elementToBeClickable(
@@ -93,28 +110,45 @@ public class TestChrome {
 //        wait.until(ExpectedConditions.stalenessOf(emailElement));
     }
 
-    private void verifyEmailDeletion(String deletedEmailSubject) {
+    private void verifyEmailDeletion(String deletedEmailThreadID) {
+//        driver.navigate().refresh();
         List<WebElement> allEmails = driver.findElements(
                 By.cssSelector("tr.zA.zE:not(.bqO)"));
-        Boolean emailExists = allEmails.stream().anyMatch(email->
-                email.findElement(By.cssSelector("span.bog"))
-                        .getText().trim().equals(deletedEmailSubject));
+
+        Boolean emailExists = allEmails.stream().anyMatch(email ->
+                Objects.equals(getEmailID(email), deletedEmailThreadID));
         Assert.assertFalse(emailExists, "Supposedly deleted email still exist");
     }
 
-//    private void verifyEmailDeletedInTrash(String deletedEmailSubject) {
-//        driver.findElement(By.cssSelector("span.CJ")).click();
-//        driver.findElement(By.cssSelector("div.TN.bzz.aHS-bnx")).click();
-//        List<WebElement> allEmails = driver.findElements(
-//                By.cssSelector("tr.zA.zE:not(.bqO)"));
-//        for (WebElement mail : allEmails){
-//            System.out.println(mail.findElement(By.cssSelector("span.bog")).getText().trim());
-//        }
-//        Boolean emailExists = allEmails.stream().anyMatch(email->
-//                email.findElement(By.cssSelector("span.bog"))
-//                        .getText().trim().equals(deletedEmailSubject));
+    private void navigateToTrash() {
+        driver.get("https://mail.google.com/mail/u/0/#trash");
+    }
+
+    private void verifyEmailDeletedInTrash(String deletedEmailThreadID) {
+//        driver.navigate().refresh();
+//        List<WebElement> allTrashEmails = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(
+//                By.cssSelector("div[role='main'] tr.zA.zE")));
+        wait.until(ExpectedConditions.titleContains("Trash"));
+        List<WebElement> allTrashEmails = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(
+                By.cssSelector("div[role='main'] tr.zA.zE")));
+
+        Boolean emailExists = allTrashEmails.stream().anyMatch(email ->
+                Objects.equals(getEmailID(email), deletedEmailThreadID));
+
+        Assert.assertTrue(emailExists, "Supposedly deleted email found in trash");
+
+    }
+
+//    @AfterClass
+//    public void tearDown() {
+//        if (driver != null) {
+//            try {
+//                Thread.sleep(5000);
+//            } catch (Exception e){
 //
-//        Assert.assertFalse(emailExists, "Supposedly deleted email still exist");
+//            }
+//            driver.quit();
+//        }
 //    }
 
 }
